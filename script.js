89f1e65bcc43f51d7e4617495dbb107328aec816
unknown <Pratik Bhangire>
2023-09-07 23:19:21 +0530

    Optimized JS & CSS code

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
 import dataFile from "./data.json" assert { type: "json" };
 let listingSection = document.getElementById("listings");
 
-listingSection.innerHTML = dataFile
-  .map(
-    (item) => `
+function createListings() {
+  listingSection.innerHTML = dataFile
+    .map(
+      (item) => `
     <div key=${item.id} id="listingCard">
-        <img id="companyProfile" src="${
-          item.logo
-        }" alt="comapny-logo" height="80" />
+        <img src="${item.logo}" alt="comapny-logo" height="80" />
         <div>
-            <div>
+            <div id="newOrFeatured">
                 <h4>${item.company}</h4>
-                <label id="labelNew">NEW!</label> 
-                <label id="labelFeatured">FEATURED</label>
+                ${item.new === true ? `<label id="labelNew">NEW!</label>` : ``} 
+                ${
+                  item.featured === true
+                    ? `<label id="labelFeatured">FEATURED</label>`
+                    : ``
+                }
             </div>
-            <h3>${item.position}</h3>
-            <div>
-                <h5 class="otherInfo">${item.postedAt}</h5>&#8226;
-                <h5 class="otherInfo">${item.contract}</h5>&#8226;
-                <h5 class="otherInfo">${item.location}</h5>
+            <h3 id="positionName">${item.position}</h3>
+            <div id="otherInfo">
+                <h5 class="otherInfoItem">${item.postedAt}</h5>&#8226;
+                <h5 class="otherInfoItem">${item.contract}</h5>&#8226;
+                <h5 class="otherInfoItem">${item.location}</h5>
             </div>
         </div>
         <div>
-            <label class="filterTab">${item.role}</label>
-            <label class="filterTab">${item.level}</label>
+            <button class="filterTab">${item.role}</button>
+            <button class="filterTab">${item.level}</button>
             ${item.languages
-              .map((language) => `<label class="filterTab">${language}</label>`)
+              .map(
+                (language) => `<button class="filterTab">${language}</button>`
+              )
               .join(" ")}
             ${item.tools
-              .map((tool) => `<label class="filterTab">${tool}</label>`)
+              .map((tool) => `<button class="filterTab">${tool}</button>`)
               .join(" ")}
         </div>
     </div>
     `
-  )
-  .join("");
+    )
+    .join("");
+}
+
+// function highlightFeaturedListing() {
+//   dataFile.map((item) =>
+//     item.featured == true
+//       ? (document.getElementById("listingCard").style.borderLeft =
+//           "5px solid hsl(180, 29%, 50%)")
+//       : ""
+//   );
+// }
+
+createListings();
